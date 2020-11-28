399fb4c665fc375d63bcbf640dcce1586f621ca8
Ben Lin <cblin@andrew.cmu.edu>
2020-11-27 20:30:12 -0600

    Added setters for gitlab comment.

--- src/main/java/org.gitlab.api/models/GitlabCommit.java
+++ src/main/java/org.gitlab.api/models/GitlabCommit.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class GitlabCommit {
+
     private String id;
     private String shortId;
     private String title;
@@ -18,10 +19,11 @@ public class GitlabCommit {
     private LocalDateTime authoredDate;
     private List<String> parentIds;
     private String status;
+    private String webUrl;
 
-    // TODO: getters for all necessary public fields
-    // TODO: public constructor
-    // TODO: withXXX() for all necessary public and modifiable fields
+    public GitlabCommit(String id) {
+        this.id = id;
+    }
 
     @Override
     public String toString() {
@@ -30,11 +32,14 @@ public class GitlabCommit {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.
+                hash(id, shortId, title, authorName, authorEmail,
+                        committerName, committerEmail, createdAt,
+                        message, committedDate, parentIds, status,
+                        webUrl);
     }
 
     @Override
-    // TODO: compare all fields for equals
     public boolean equals(Object o) {
         if (o == this) {
             return true;
@@ -43,6 +48,74 @@ public class GitlabCommit {
             return false;
         }
         GitlabCommit commit = (GitlabCommit) o;
-        return commit.id == this.id;
+        return Objects.equals(commit.id, this.id) &&
+                Objects.equals(commit.shortId, this.shortId) &&
+                Objects.equals(commit.title, this.title) &&
+                Objects.equals(commit.authorName, this.authorName) &&
+                Objects.equals(commit.authorEmail, this.authorEmail) &&
+                Objects.equals(commit.committerName, this.committerName) &&
+                Objects.equals(commit.committerEmail, this.committerEmail) &&
+                Objects.equals(commit.createdAt, this.createdAt) &&
+                Objects.equals(commit.message, this.message) &&
+                Objects.equals(commit.committedDate, this.committedDate) &&
+                Objects.equals(commit.authoredDate, this.authoredDate) &&
+                Objects.equals(commit.parentIds, this.parentIds) &&
+                Objects.equals(commit.status, this.status) &&
+                Objects.equals(commit.webUrl, webUrl);
+    }
+
+    /*
+     * Getters
+     */
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getAuthorEmail() {
+        return authorEmail;
+    }
+
+    public String getCommitterName() {
+        return committerName;
+    }
+
+    public String getCommitterEmail() {
+        return committerEmail;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getCommittedDate() {
+        return committedDate;
+    }
+
+    public LocalDateTime getAuthoredDate() {
+        return authoredDate;
+    }
+
+    public List<String> getParentIds() {
+        return parentIds;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    private String getWebUrl() {
+        return webUrl;
     }
 }
