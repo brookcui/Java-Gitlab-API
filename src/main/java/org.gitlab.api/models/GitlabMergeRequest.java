0db9c081b2a093e4db005e399a7f023e90e6250a
Ben Lin <cblin@andrew.cmu.edu>
2020-11-29 11:14:45 -0600

    Added json tags for merge request.

--- src/main/java/org.gitlab.api/models/GitlabMergeRequest.java
+++ src/main/java/org.gitlab.api/models/GitlabMergeRequest.java
@@ -1,12 +1,74 @@
 package org.gitlab.api.models;
 
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.gitlab.api.core.Pagination;
+import org.gitlab.api.http.GitlabHTTPRequestor;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
-public class GitlabMergeRequest {
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY,
+        getterVisibility = JsonAutoDetect.Visibility.NONE
+)
+public class GitlabMergeRequest extends GitlabComponent{
+    private int id; // required, url of the project
+    private int iid;
+    private int projectId;
+
+    @JsonProperty(value = "author", access = JsonProperty.Access.WRITE_ONLY)
+    private GitlabUser author;
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("state")
+    private String state;
+    @JsonProperty("assignees")
+    private List<GitlabUser> assignees;
+    @JsonProperty(value = "upvotes", access = JsonProperty.Access.WRITE_ONLY)
+    private int upvotes;
+    @JsonProperty(value = "downvotes", access = JsonProperty.Access.WRITE_ONLY)
+    private int downvotes;
+    @JsonProperty(value = "merge_requests_count", access = JsonProperty.Access.WRITE_ONLY)
+    private int mergeRequestCount;
+
+    private String title; // required
+    @JsonProperty(value = "updated_at", access = JsonProperty.Access.WRITE_ONLY)
+    private LocalDateTime updatedAt;
+    @JsonProperty(value = "created_at", access = JsonProperty.Access.WRITE_ONLY)
+    private LocalDateTime createdAt;
+    @JsonProperty(value = "closed_at", access = JsonProperty.Access.WRITE_ONLY)
+    private LocalDateTime closedAt;
+    @JsonProperty(value = "closed_by", access = JsonProperty.Access.WRITE_ONLY)
+    private GitlabUser closedBy;
+    @JsonProperty(value = "subscribed", access = JsonProperty.Access.WRITE_ONLY)
+    private boolean subscribed;
+    @JsonProperty(value = "web_url", access = JsonProperty.Access.WRITE_ONLY)
+    private String webUrl;
+
+    private String targetBranch; // required
+    private String sourceBranch; // required
+
+    @JsonIgnore
+    private final GitlabProject project;
+
+    public GitlabMergeRequest(@JsonProperty("project") GitlabProject project,
+                              @JsonProperty("source_branch") String sourceBranch,
+                              @JsonProperty("target_branch") String targetBranch,
+                              @JsonProperty("title") String title) {
+        this.project = project;
+        this.sourceBranch = sourceBranch;
+        this.targetBranch = targetBranch;
+        this.title = title;
+    }
+
+    @Override
+    public GitlabMergeRequest withHTTPRequestor(GitlabHTTPRequestor requestor) {
+        super.withHTTPRequestor(requestor);
+        return this;
+    }
 
     // create a new gitlab issue
     public GitlabMergeRequest create() throws IOException {
