d40bfd9ec68109918f9488ff1390676af2254999
Rong Cui <brookcui97@gmail.com>
2020-12-14 01:16:11 -0500

    Rewrite test for Pagination

--- src/test/java/org/gitlab/api/test/PaginationTest.java
+++ src/test/java/org/gitlab/api/test/PaginationTest.java
@@ -1,5 +1,6 @@
-package org.gitlab.api.core;
+package org.gitlab.api.test;
 
+import org.gitlab.api.Pagination;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,27 +15,27 @@ class PaginationTest {
     }
 
     @Test
-    void getPagination() {
-        Pagination pagination = Pagination.getPagination(1, 5);
+    void of() {
+        Pagination pagination = Pagination.of(1, 5);
         assertEquals(1, pagination.getPageNumber());
         assertEquals(5, pagination.getPageSize());
         assertDoesNotThrow(() -> {
-            Pagination.getPagination(1, 100);
+            Pagination.of(1, 100);
         });
         assertDoesNotThrow(() -> {
-            Pagination.getPagination(100, 1);
+            Pagination.of(100, 1);
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            Pagination.getPagination(-1, 5);
+            Pagination.of(-1, 5);
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            Pagination.getPagination(1, 0);
+            Pagination.of(1, 0);
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            Pagination.getPagination(1, 200);
+            Pagination.of(1, 200);
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            Pagination.getPagination(-1, -1);
+            Pagination.of(-1, -1);
         });
     }
 
@@ -42,7 +43,7 @@ class PaginationTest {
     void getPageNumber() {
         Pagination defaultPagination = Pagination.getDefaultPagination();
         assertEquals(0, defaultPagination.getPageNumber());
-        Pagination pagination = Pagination.getPagination(1, 5);
+        Pagination pagination = Pagination.of(1, 5);
         assertEquals(1, pagination.getPageNumber());
     }
 
@@ -50,7 +51,7 @@ class PaginationTest {
     void getPageSize() {
         Pagination defaultPagination = Pagination.getDefaultPagination();
         assertEquals(20, defaultPagination.getPageSize());
-        Pagination pagination = Pagination.getPagination(1, 5);
+        Pagination pagination = Pagination.of(1, 5);
         assertEquals(5, pagination.getPageSize());
     }
 }
